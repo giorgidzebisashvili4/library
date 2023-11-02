1eeb7e7d2416c5bcef32608e076d581de5898bed
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2023-11-02 15:16:19 +0400

    add minimum form desine

--- javascript.js
+++ javascript.js
@@ -5,14 +5,19 @@ function book(title, author, pages, read) {
     this.author = author
     this.pages = pages
     this.read = read
-    this.info = function(){
-        return (title+" "+author+", "+pages+", "+read)
-    }
+    // this.info = function(){
+    //     return (title+" "+author+", "+pages+", "+read)
+    // }
 }
 
 const theHobbit = new book("The Hobbit","by J.R.R. Tolkien","295 pages","not read yet" )
-console.log( theHobbit.info())
-
+myLibrary.push(theHobbit) 
+console.log(myLibrary)
+const theHobbit2 = new book("The Hobbit2","by J.R.R. Tolkien","300 pages","yes" )
+const theHobbit3 = new book("The Hobbit3","by J.R.R. Tolkien","350 pages","no" )
+myLibrary.push(theHobbit2)
+myLibrary.push(theHobbit3) 
+console.log(myLibrary)
 function addBookToLibrary() {
     // do stuff here
   }
